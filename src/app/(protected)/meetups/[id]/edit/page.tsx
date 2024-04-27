e39107075e28cf9189f8c583ee2f154e026e1686
CoderN-P <neel.parpia@gmail.com>
2024-04-27 13:20:51 -0700

    Fix more ts issues

--- src/app/(protected)/meetups/[id]/edit/page.tsx
+++ src/app/(protected)/meetups/[id]/edit/page.tsx
@@ -1,10 +1,6 @@
-import Sidebar from "@/app/components/sidebar";
-import {defaultUser} from "@/types";
 
 export default function EditMeetup(){
     return (
-        <div className="flex flex-row bg-neutral-100 dark:bg-black h-screen w-screen">
-            <Sidebar user={defaultUser} active="meetups"/>
-        </div>
+        <div></div>
     )
 }
