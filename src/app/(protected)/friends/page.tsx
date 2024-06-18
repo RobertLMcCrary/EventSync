77776a6cd915e30a8c349deca4102b07054e792b
CoderN-P <neel.parpia@gmail.com>
2024-06-17 19:21:17 -0700

    Added code for accepting/rejecting friend requests

--- src/app/(protected)/friends/page.tsx
+++ src/app/(protected)/friends/page.tsx
@@ -1,7 +1,7 @@
 // TODO: Completed Friends Page
 "use client";
 import { useUser, sessionContext } from "@/app/providers";
-import {Button, Input, useDisclosure} from "@nextui-org/react";
+import {Button, Input, Tab, Tabs, useDisclosure} from "@nextui-org/react";
 import {MagnifyingGlassIcon, PlusIcon} from "@heroicons/react/24/solid";
 import {UserCard} from "@/app/components/UserCard";
 import AddFriendModal from "@/app/components/AddFriendModal";
@@ -15,48 +15,179 @@ export default function Friends() {
     const session = useContext(sessionContext);
     const [friends, setFriends] = useState<(User | null)[]>([null, null, null, null, null, null, null]);
     const [incomingRequests, setIncomingRequests] = useState<(User | null)[]>([null, null, null, null]);
+    const [outgoingRequests, setOutgoingRequests] = useState<(User | null)[]>([null, null, null, null]);
+
+
+    console.log(user);
 
     useEffect(() => {
         if (!user) return;
-        if (friends[0] !== null) return;
 
-        const friendPromises = Promise.all(user.friends.map(async (friendID) => {
-            const res = await fetch(`/api/user/${friendID}`, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${session.session.token}`
-                }
+        if (friends.includes(null)) {
+            setFriends([]);
+            const friendPromises = Promise.all(user.friends.map(async (friendID) => {
+                const res = await fetch(`/api/user/${friendID}`, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${session.session.token}`
+                    }
+                });
+                if (!res.ok) return null;
+                return await res.json();
+            }));
+
+            friendPromises.then((friendsData) => {
+                let friendsDataFiltered = friendsData.filter((friend) => friend !== null);
+                setFriends(friendsDataFiltered.map((friend) => friend as User));
             });
-            if (!res.ok) return null;
-            return await res.json();
-        }));
+        }
 
-        friendPromises.then((friendsData) => {
-            let friendsDataFiltered = friendsData.filter((friend) => friend !== null);
-            setFriends(friendsDataFiltered.map((friend) => friend as User));
+        if (incomingRequests.includes(null)){
+            setIncomingRequests([]);
+            const incomingRequestsPromises = Promise.all(user.incomingFriendRequests.map(async (requestID) => {
+                const res = await fetch(`/api/user/${requestID}`, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${session.session.token}`
+                    }
+                });
+                if (!res.ok) return null;
+                return await res.json();
+            }));
+
+            incomingRequestsPromises.then((incomingRequestsData) => {
+                let incomingRequestsDataFiltered = incomingRequestsData.filter((request) => request !== null);
+                setIncomingRequests(incomingRequestsDataFiltered.map((request) => request as User));
+            });
+        }
+
+        if (outgoingRequests.includes(null)) {
+            setOutgoingRequests([]);
+            const outgoingRequestsPromises = Promise.all(user.outgoingFriendRequests.map(async (requestID) => {
+                const res = await fetch(`/api/user/${requestID}`, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${session.session.token}`
+                    }
+                });
+                if (!res.ok) return null;
+                return await res.json();
+            }));
+
+            outgoingRequestsPromises.then((outgoingRequestsData) => {
+                let outgoingRequestsDataFiltered = outgoingRequestsData.filter((request) => request !== null);
+                setOutgoingRequests(outgoingRequestsDataFiltered.map((request) => request as User));
+            });
+        }
+
+    }, [user, friends, session.session.token, incomingRequests, outgoingRequests]);
+
+
+    async function acceptFriendRequest(friend: User) {
+        if (!user) return;
+
+        const res = await fetch(`/api/user/${user._id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${session.session.token}`,
+            },
+            body: JSON.stringify({
+                update: {
+                    $pull: {
+                        incomingFriendRequests: friend._id
+                    },
+                    $push: {
+                        friends: friend._id
+                    }
+                }
+            })
         });
 
-        if (incomingRequests[0] !== null) return;
-
-        const incomingRequestsPromises = Promise.all(user.incomingFriendRequests.map(async (requestID) => {
-            const res = await fetch(`/api/user/${requestID}`, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${session.session.token}`
+        const res2 = await fetch(`/api/user/${friend._id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${session.session.token}`,
+            },
+            body: JSON.stringify({
+                update: {
+                    $pull: {
+                        outgoingFriendRequests: user._id
+                    },
+                    $push: {
+                        friends: user._id
+                    }
                 }
-            });
-            if (!res.ok) return null;
-            return await res.json();
-        }));
-
-        incomingRequestsPromises.then((incomingRequestsData) => {
-            let incomingRequestsDataFiltered = incomingRequestsData.filter((request) => request !== null);
-            setIncomingRequests(incomingRequestsDataFiltered.map((request) => request as User));
+            })
         });
 
-    }, [user, friends, session.session.token, incomingRequests]);
+        const res3 = await fetch(`/api/notification/create`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${session.session.token}`,
+            },
+            body: JSON.stringify({
+                initiator: user._id,
+                receiver: friend._id,
+                type: 4,
+            })
+        });
+    }
+
+    async function declineFriendRequest(friend: User) {
+        if (!user) return;
+
+        const res = await fetch(`/api/user/${user._id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${session.session.token}`,
+            },
+            body: JSON.stringify({
+                update: {
+                    $pull: {
+                        incomingFriendRequests: friend._id
+                    }
+                }
+            })
+        });
+
+        const res2 = await fetch(`/api/user/${friend._id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${session.session.token}`,
+            },
+            body: JSON.stringify({
+                update: {
+                    $pull: {
+                        outgoingFriendRequests: user._id
+                    }
+                }
+            })
+        });
+
+        const res3 = await fetch(`/api/notification/create`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${session.session.token}`,
+            },
+            body: JSON.stringify({
+                initiator: user._id,
+                receiver: friend._id,
+                type: 5,
+            })
+        });
+
+        await updateUser();
+        return;
+    }
 
 
     return (
@@ -67,22 +198,37 @@ export default function Friends() {
         </Button>
 
           <Input startContent={ <MagnifyingGlassIcon className="text-gray-400 h-6 w-6"/>} className="absolute top-4 right-16 w-64" placeholder="Search"/>
-        <div className="flex w-screen h-full flex-row">
-            <div className="flex w-2/3 flex-wrap p-4 gap-4 overflow-y-scroll h-full">
+        <div className="flex w-full h-full flex-row">
+            <div className={"flex w-2/3 " + (friends.length === 0 ? "items-center justify-center" : "flex-wrap") + " p-4 gap-4 overflow-y-scroll h-full"}>
+                {friends.length === 0 ? <p className="text-gray-500 text-lg">No friends yet</p> : null}
                 {friends.map((friend, index) => (
                     <UserCard user={friend} key={index}/>
                 ))}
             </div>
             <div className="w-1/3">
-                <div className="p-4 bg-white dark:bg-stone-900 h-full shadow-md">
-                    <h1 className="text-xl font-semibold">Friend Requests</h1>
-                    <div className="flex flex-col gap-4 mt-4">
-                        {incomingRequests.map((request, index) => (
-                            <div className="dark:border dark:border-gray-800 rounded-xl">
-                                <UserCard user={request} key={index}/>
+                <div className="p-4 bg-white w-full dark:bg-stone-900 overflow-y-scroll max-h-full h-full shadow-md">
+                    <h1 className="text-xl font-semibold mb-4 ">Friend Requests</h1>
+                    <Tabs className="w-full">
+                        <Tab title="Received">
+                            <div className="flex flex-col gap-4 mt-4">
+                                {incomingRequests.map((request, index) => (
+                                    <div key={index} className="dark:border dark:border-gray-800 rounded-xl">
+                                        <UserCard user={request} key={index}/>
+                                    </div>
+                                ))}
                             </div>
-                        ))}
-                    </div>
+                        </Tab>
+
+                        <Tab title="Sent">
+                            <div className="flex flex-col gap-4 mt-4">
+                                {outgoingRequests.map((request, index) => (
+                                    <div key={index} className="dark:border dark:border-gray-800 rounded-xl">
+                                        <UserCard user={request} key={index}/>
+                                    </div>
+                                ))}
+                            </div>
+                        </Tab>
+                    </Tabs>
                 </div>
             </div>
         </div>
